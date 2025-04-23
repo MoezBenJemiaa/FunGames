1bfa4a92c9d9f64fe2db4b912797c17c9776e1c5
Moez <moez.benjemiaa@etudiant-fst.utm.tn>
2025-04-23 11:25:02 +0100

    404 page + root for the games

--- gamesite/src/App.jsx
+++ gamesite/src/App.jsx
@@ -1,14 +1,50 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useParams, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 import HomePage from "./home/home";
 import LoginPage from "./login/YetiLogin";
 import Xo from "./xo/xoboard";
 import Hungman from "./hangman/hangman";
+import NotFoundPage from "./404/404";
+
+
+function GameRouteHandler() {
+  const { roomCode } = useParams();
+  const [component, setComponent] = useState(<div>Loading...</div>);
+
+  useEffect(() => {
+    const fetchRoom = async () => {
+      try {
+        const res = await fetch(`http://localhost:5000/room/${roomCode}`);
+        if (!res.ok) throw new Error("Room not found");
+        const room = await res.json();
+
+        switch (room.game) {
+          case "XO":
+            setComponent(<Xo />);
+            break;
+          case "Hangman":
+            setComponent(<Hungman />);
+            break;
+          default:
+            setComponent(<NotFoundPage />);
+        }
+      } catch (err) {
+        setComponent(<NotFoundPage />);
+      }
+    };
+
+    fetchRoom();
+  }, [roomCode]);
+
+  return component;
+}
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/game/:roomCode" element={<Hungman/>/*TODO*/} />
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/game/:roomCode" element={<GameRouteHandler />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
